1e168f88b118d1e70ee57c6b7ad323038d33f274
knanak <>
2023-03-06 07:36:56 +0000

    description

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 # 알파벳 재정렬 문제 : 대문자는 오름차순으로 정렬하고, 숫자는 모두 더하기
 
 import string
-s=input()
-s=s.replace(' ', '')
+
+s = input()
+s = s.replace(' ', '')
 print(s)
 
-ch=''
-result=0
+ch = ''
+result = 0
 
 for i in s:
-  if i.isalpha():
-    ch+=i
-    new_ch = sorted(ch)
+    if i.isalpha():
+        ch += i
+        new_ch = sorted(ch)
 
-  else:
-    i=int(i)
-    
-    result+=i
+    else:
+        i = int(i)
 
+        result += i
 
-
-new=''
+new = ''
 for i in new_ch:
-  new+=i
-  
+    new += i
+
 print(new, result, sep='')
