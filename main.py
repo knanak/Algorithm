3df9153a8eafb8ef99ca1e32c954c4c4da9310d0
knanak <>
2023-03-08 04:19:15 +0000

    factorial

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-# 알파벳 재정렬 문제 : 대문자는 오름차순으로 정렬하고, 숫자는 모두 더하기
+# 팩토리얼 : 1부터 n까지의 곱
 
-import string
+# 함수 범위 주의
+# result=1로 함수 밖에서 할당 할 경우, 함수 안에서 할당 전 사용 오류가 남
 
-s = input()
-s = s.replace(' ', '')
-print(s)
 
-ch = ''
-result = 0
+def f(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+        # return값이 for 반복문 안에 있는 경우, 첫번째 반복문을 끝내면 return하기 때문에 설정된 range만큼 반복되지 못한채 종료됨
+        # return result
 
-for i in s:
-    if i.isalpha():
-        ch += i
-        new_ch = sorted(ch)
+    return result
 
-    else:
-        i = int(i)
 
-        result += i
+print(f(5))
 
-new = ''
-for i in new_ch:
-    new += i
+# 2) 재귀함수로 구현
 
-print(new, result, sep='')
+
+def f_self(n):
+    if n <= 1:
+        return 1
+    return n * f_self(n - 1)
+
+
+print(f_self(5))
