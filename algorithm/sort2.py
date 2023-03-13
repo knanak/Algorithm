378660e3767368c8f9cc7683ed510435323870f7
knanak <knanak10@gmail.com>
2023-03-13 04:38:14 +0000

    sort2

--- algorithm/sort2.py
+++ algorithm/sort2.py
@@ -4,14 +4,13 @@ n, k = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 
-# 1) min(), max() 이용
+# 1) min(), max() 함수 이용
 # for i in range(k):
 #     am = a.index(min(a))
 #     bm = b.index(max(b))
 #     a[am], b[bm] = b[bm], a[am]
 
 # print(sum(a))
-
 
 # 2) .sort()이용
 a.sort()
