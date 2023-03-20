382760d210859bd320737e0e336dfe8e7c3fa733
knanak <knanak10@gmail.com>
2023-03-20 10:14:31 +0000

    quick

--- algorithm/main.py
+++ algorithm/main.py
@@ -1,37 +1,40 @@
-# stack 2개를 이용해 queue만들기 : 선입선출
-class Queue(object):
-    def __init__(self):
-        self.inputstack = []
-        self.outputstack = []
-
-    def input(self, element):
-        self.inputstack.append(element)
-
-    def output(self):
-        if not self.outputstack:
-            while self.inputstack:
-                self.outputstack.append(self.inputstack.pop())
-        return self.outputstack.pop()
+# 퀵정렬 구현 : pivot을 기준으로 앞 뒤를 쪼개면서 정렬, 재귀적
+arr = [5, 4, 3, 8, 0, 1, 2]
 
 
-# queue 2개를 이용해 stack 만들기 : top(마지막에 추가된 데이터)부터 출력
-import queue
+def quick1(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[0]
+    tail = arr[1:]
+
+    left = [x for x in tail if x <= pivot]  ## pivot보다 작으면 왼쪽으로 정렬
+    right = [x for x in tail if x > pivot]
+
+    return quick1(left) + [pivot] + quick1(right)
 
 
-class Stack(object):
-    def __init__(self):
-        self.inQue = queue.Queue()
-        self.outQue = queue.Queue()
+def quick2(arr, start, end):
+    if start >= end:
+        return
 
-    def input(self, element):
-        self.inQue.put(element)
+    pivot = start
+    left = start + 1
+    right = end
 
-    def output(self):
-        while self.inQue.qsize() > 1:
-            self.outQue.put(self.inQue.get())
+    while (left <= right):
+        while (left <= end and arr[left] <= arr[pivot]):
+            left += 1
+        while (right >= start and arr[right] <= arr[pivot]):
+            right -= 1
 
-        temp = self.inQue
-        self.inQue = self.outQue
-        self.outQue = temp
+        if (right < left):
+            arr[right], arr[pivot] = arr[pivot], arr[right]
+        else:
+            arr[right], arr[left] = arr[left], arr[right]
 
-        return self.outQue.get()
+    quick2(arr, start, right - 1)
+    quick2(arr, right + 1, end)
+
+
+quick2(arr, 0, len(arr) - 1)
---
+++ algorithm/stackQue.py
@@ -0,0 +1,37 @@
+# stack 2개를 이용해 queue만들기 : 선입선출
+class Queue(object):
+    def __init__(self):
+        self.inputstack = []
+        self.outputstack = []
+
+    def input(self, element):
+        self.inputstack.append(element)
+
+    def output(self):
+        if not self.outputstack:
+            while self.inputstack:
+                self.outputstack.append(self.inputstack.pop())
+        return self.outputstack.pop()
+
+
+# queue 2개를 이용해 stack 만들기 : top(마지막에 추가된 데이터)부터 출력
+import queue
+
+
+class Stack(object):
+    def __init__(self):
+        self.inQue = queue.Queue()
+        self.outQue = queue.Queue()
+
+    def input(self, element):
+        self.inQue.put(element)
+
+    def output(self):
+        while self.inQue.qsize() > 1:
+            self.outQue.put(self.inQue.get())
+
+        temp = self.inQue
+        self.inQue = self.outQue
+        self.outQue = temp
+
+        return self.outQue.get()
