e1107ac7dac894ee49da3f76153fc0dd5c603f54
knanak <98734030+knanak@users.noreply.github.com>
2023-03-23 06:36:44 +0000

    sort_final

--- algorithm/main.py
+++ algorithm/main.py
@@ -17,6 +17,7 @@ for i in range(len(list)):
 print(list)
 
 # c. 삽입정렬 : 현재 데이터를 왼쪽값과 비교하여 적절한 위치에 삽입. 적절한 위치라고 판단될 경우에만 스왑이 일어나기 때문에 o(n^2)중에서는 가장 빠르다. 정렬이 될떄마다 왼쪽에 있는 데이터는 이미 정렬된 것으로 간주한다.
+
 for i in range(1, len(list)):
     min = i
     for j in range(i, 0, -1):
@@ -48,6 +49,74 @@ def quick(arr):
 
     return quick(left) + [pivot] + quick(right)
 
+
 quick(list)
 
-def quick2()
+
+def quick2(arr, start, end):
+    if start >= end:
+        return
+
+    pivot = start
+    left = start + 1
+    right = end
+
+    while (left <= right):
+        while (arr[pivot] >= arr[left] and left <= end):
+            left += 1
+
+        # right는 pivot과 같을 수 없기 때문에 right != start
+        while (arr[pivot] <= arr[right] and right > start):
+            right -= 1
+
+        if (left > right):
+            arr[right], arr[pivot] = arr[pivot], arr[right]
+        else:
+            arr[right], arr[left] = arr[left], arr[right]
+
+    quick2(arr, start, right - 1)
+    quick2(arr, right + 1, end)
+
+
+quick2(list, 0, len(list) - 1)  ## -1이 없으면 69번째에서 list index out of range
+print(list)
+
+# e. 머지정렬 : 정렬되지 않은 배열을 하나의 데이터가 남을때까지 분할한 뒤에 합치면서 정렬한다. 즉, n개의 데이터를 /2 하여 하나의 데이터가 남을 때까지 재귀적으로 나누고, 이를 왼쪽과 오른쪽으로 구분해 새로운 리스트에 담는다. 왼쪽과 오른쪽의 데이터를 병합하며 정렬한다. 별도의 pivot값을 설정할 필요없이 배열의 중간위치의 값을 기준으로 나누고 합치기 때문에 항상 o(n logn)의 시간복잡도를 보장한다. 하지만 정렬한 값을 저장할 추가적인 배열이 필요하기에 메모리가 없거나, 메모리 효율을 추구할 경우에는 바람직하지 않다.
+
+
+def divide(arr):
+    if len(arr) <= 1:
+        return arr  ## 재귀의 종료 조건은 맨처음에 와야 무한반복을 값을 거를 수 있다.
+
+    mid = len(arr) // 2
+    left = arr[mid:]
+    right = arr[:mid]
+
+    return merge(divide(left), divide(right))
+
+
+def merge(left, right):
+    i = 0
+    j = 0
+    sorted = []
+
+    while (i < len(left) and j < len(right)):
+        if left[i] <= right[j]:
+            sorted.append(left[i])
+            i += 1
+        else:
+            sorted.append(right[j])
+            j += 1
+
+    while i < len(left):
+        sorted.append(left[i])
+        i += 1
+
+    while j < len(right):
+        sorted.append(right[j])
+        j += 1
+
+    return sorted
+
+
+print(divide(list))
