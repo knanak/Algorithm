13bb2c2742f7f3599c13260f626ecbfd6a7b8b0d
knanak <98734030+knanak@users.noreply.github.com>
2023-03-25 10:15:55 +0000

    listIndex

--- algorithm/main.py
+++ algorithm/main.py
@@ -1,42 +1,13 @@
-# how to change character
-h = 'happy birth day'
-print(h[:4] + h[5:])
+# 가장 큰 수를 찾고 그때의 index를 list로 반환하기
+list = [2, 5, 1, 7, 5, 8]
 
 
-# 프로그래머스 더 맵게 문제
-# 1. sort() : o(n logn)으로 인해 시간초과
-def solution(s, k):
-    s.sort()
-    cnt = 0
-
-    while s[0] < k:
-        new = s.pop(0) + s.pop(0) * 2
-        s.append(new)
-        s.sort()
-        cnt += 1
-        if len(s) == 1 and s[0] < k:
-            return -1
-    return cnt
+# 1. list에서 값으로 index 찾기
+def solution(array):
+    return [max(array), list.index(max(array))]
 
 
-# 1. heapq : o(logn)으로 성공
-import heapq
-
-
-def solution2(s, k):
-    heap = []
-    for i in s:
-        heapq.heappush(heap, i)
-    cnt = 0
-    while heap[0] < k:
-        new = heapq.heappop(heap) + heapq.heappop(heap) * 2
-        heapq.heappush(heap, new)
-        cnt += 1
-        if len(heap) == 1 and heap[0] < k:
-            return -1
-    return cnt
-
-
-s = [1, 3, 4, 6]
-print(solution(s, 7))
-print(solution2(s, 7))
+# 2. sort()와 sorted()의 차이
+def solution2(array):
+    a = sorted(array)
+    return [a[-1], array.index(a[-1])]
---
+++ algorithm/sort_heapq.py
@@ -0,0 +1,55 @@
+# how to change character
+h = 'happy birth day'
+print(h[:4] + h[5:])
+
+
+# 프로그래머스 더 맵게 문제
+# 1. sort() : o(n logn)으로 인해 시간초과
+def solution(s, k):
+    s.sort()
+    cnt = 0
+
+    while s[0] < k:
+        new = s.pop(0) + s.pop(0) * 2
+        s.append(new)
+        s.sort()
+        cnt += 1
+        if len(s) == 1 and s[0] < k:
+            return -1
+    return cnt
+
+
+# 1. heapq : o(logn)으로 성공
+import heapq
+
+
+def solution2(s, k):
+    heap = []
+    for i in s:
+        heapq.heappush(heap, i)
+    cnt = 0
+    while heap[0] < k:
+        new = heapq.heappop(heap) + heapq.heappop(heap) * 2
+        heapq.heappush(heap, new)
+        cnt += 1
+        if len(heap) == 1 and heap[0] < k:
+            return -1
+    return cnt
+
+
+s = [1, 3, 4, 6]
+print(solution(s, 7))
+print(solution2(s, 7))
+
+
+# heap 정렬
+def heapSort(list):
+    heapq.heapify(list)
+
+    new = []
+    while list:
+        new.append(heapq.heappop(list))
+    return new
+
+
+print(heapSort([3, 2, 1]))
