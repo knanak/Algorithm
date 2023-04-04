761f201089c3f1cec299556be1fa402133375d61
knanak <98734030+knanak@users.noreply.github.com>
2023-04-04 10:55:33 +0000

    strCount

--- algorithm/main.py
+++ algorithm/main.py
@@ -1,55 +1,55 @@
-# 가까운 수 구하기. 차가 같을 때는 작은 수를 선택
-arr = [10, 11, 12]
-n = 13
+# 7의 갯수 : list를 str으로 변경해서 count()하기
+arr = [7, 77, 17]
 
 
-# 1. elif를 통한 연속적인 비교문 처리
-def solution(arr, n):
-    arr.append(n)
-    arr.sort()
-    i = arr.index(n)
-
-    # n의 위치가 양 끝일 경우
-    if i == len(arr) - 1:
-        return arr[i - 1]
-    elif i == 0:
-        return arr[1]
-    # n의 위치가 중간일 경우
-    elif arr[i + 1] - arr[i] < arr[i] - arr[i - 1]:
-        return arr[i + 1]
-    elif arr[i + 1] - arr[i] >= arr[i] - arr[i - 1]:
-        return arr[i - 1]
-
-
-print(solution(arr, n))
-
-
-# 2. list.sorted(key=lambda 인자 : 표현식)
-def solution2(arr, n):
-    arr.sort(key=lambda x: (abs(n - x), x))  #sort는 원본을 변형
-    return arr[0]
-
-
-print(solution2(arr, n))
-
-
-# 3. min()
-def solution3(arr, n):
-    arr.sort()
-    temp = []
+# 1. 이중 반복문 : for in
+def solution(arr):
+    chr = []
     for i in arr:
-        temp.append(abs(i - n))
-    return arr[temp.index(min(temp))]
+        chr.append(str(i))
+
+    result = 0
+    for i in chr:
+        for j in i:
+            if j == '7':
+                result += 1
+    return result
 
 
-print(solution3(arr, n))
+print(solution(arr))
 
 
-# 4. dict.item()
-def solution4(arr, n):
-    dict = {i: abs(i - n) for i in arr}
-    # sorted(list, key)는 정렬된 것을 반환
-    return sorted(dict.items(), key=lambda x: (x[1], x[0]))[0][0]
+# 2. list/str.count(값)
+def solution2(arr):
+    chr = ''
+    for i in arr:
+        chr += str(i)
+    return chr.count('7')
 
 
-print(solution4(arr, n))
+print(solution2(arr))
+
+
+## list를 string으로 변경하는 방법
+## 1. ''.join(list)
+def solution3(arr):
+    return ''.join(map(str, arr)).count('7')
+
+
+print(solution3(arr))
+
+
+## 2. str(list)
+def solution4(arr):
+    return str(arr).count('7')
+
+
+print(solution4(arr))
+
+
+## 4. 반복문
+def solution5(arr):
+    return sum([str(i).count('7') for i in arr])
+
+
+print(solution5(arr))
