23bd9c447ee6f52fe7042954846f0c9526ed95b0
knanak <98734030+knanak@users.noreply.github.com>
2023-04-08 11:58:14 +0000

    factorial

--- algorithm/main.py
+++ algorithm/main.py
@@ -1,26 +1,61 @@
-# 문자열 계산 : 문자열의 +, -를 이용하여 계산
-my_string = "3 + 4"
+# 팩토리얼 : i! <= n일때, i의 최대값 구하기
+n = 3628800
 
 
-# 1. range(시작, 끝, 간격)
-def solution(my_string):
-    s = my_string.split()
-    result = int(s[0])
-    for i in range(1, len(s), 2):
-        if s[i] == '+':
-            result += int(s[i + 1])
-        else:
-            result -= int(s[i + 1])
-
-    return result
+# 1. 반복문으로 곱해주기
+def solution(n):
+    answer = 1
+    for i in range(2, 11):
+        answer *= i  # answer < n이면, 계속 곱해주기
+        if answer > n:
+            return i - 1
+        elif answer == n:
+            return i
 
 
-print(solution(my_string))
+print(solution(n))
+
+# 2. 끝부터 빼주는 방식
+from math import factorial
 
 
-# 2. str.replace('-를', '-로')
-def solution2(my_string):
-    return sum([int(i) for i in my_string.replace('-', '+-').split('+')])
+def solution2(n):
+    k = 10
+    while n < factorial(k):
+        k -= 1
+    return k  ## factorial(k) >= n이 될때 while문을 빠져나옴
 
 
-print(solution2(my_string))
+print(solution2(n))
+
+
+# 3. 재귀함수
+def fact(n):
+    if n > 1:
+        return n * factorial(n - 1)
+    else:
+        return 1
+
+
+def solution3(n):
+    k = 10
+    while n < fact(k):
+        k -= 1
+    return k  ## factorial(k) >= n이 될때 while문을 빠져나옴
+
+
+print(solution3(n))
+
+
+# 4. while과 +=으로 for 반복문 대신하기
+def solution4(n):
+    answer = 1
+    fact = 1
+    while fact <= n:
+        answer += 1
+        fact = fact * answer
+    answer = answer - 1
+    return answer
+
+
+print(solution4(n))
---
+++ algorithm/strReplace.py
@@ -0,0 +1,32 @@
+# 문자열 계산 : 문자열의 +, -를 이용하여 계산
+my_string = "3 + 4"
+
+
+# 1. range(시작, 끝, 간격)
+def solution(my_string):
+    s = my_string.split()
+    result = int(s[0])
+    for i in range(1, len(s), 2):
+        if s[i] == '+':
+            result += int(s[i + 1])
+        else:
+            result -= int(s[i + 1])
+
+    return result
+
+
+print(solution(my_string))
+
+
+# 2. str.replace('-를', '-로')
+def solution2(my_string):
+    return sum([int(i) for i in my_string.replace('-', '+-').split('+')])
+
+
+print(solution2(my_string))
+
+# 3. eval("문자열 표현식 계산")
+def solution3(my_string):
+    return eval(my_string)
+
+print(solution3(my_string))
