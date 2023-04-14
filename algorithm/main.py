bd816a841bf8d3477c34e37994b517f900a9396c
knanak <98734030+knanak@users.noreply.github.com>
2023-04-14 11:05:39 +0000

    result[-1]+=1

--- algorithm/main.py
+++ algorithm/main.py
@@ -1,63 +1,52 @@
-# 연속으로 중복된 숫자 제거하기
-arr = [1, 1, 3, 3, 0, 1, 1]  # 답 : [1,3,0,1]
+# 배포순서와 개발속도를 고려하여 배포되는 숫자 구하기
+progresses = [95, 90, 99, 99, 80, 99]
+speeds = [1, 1, 1, 1, 1, 1]  # 정답 : [1, 3, 2]
+
+## (1) 현재까지 진행률과 개발속도를 고려해서 배포까지 남은 날짜 구하기
+
+## (2) 남은 날짜와 배포순서를 고려하여 배포하기. 배포순서를 고려하여 앞에서부터 처리되야 하기 때문에 que 자료구조를 사용하거나, list.pop(0)을 사용함
+
+# 1. math.ceil() 사용하여 남은 날짜 구하고, que 자료구조를 사용해 비교하기
+import queue
+import math
 
 
-# 1. stack 이용
-def solution(arr):
-    result = []
-    for i in arr:
-        if result[-1:] != [i]:
-            result.append(i)
+def solution(progresses, speeds):
+    q = queue.Queue()
+    for i in range(len(progresses)):
+        day = math.ceil((100 - progresses[i]) / speeds[i])
+        q.put(day)
+
+    first = q.get()
+    result = [1]
+    for i in range(1, len(progresses)):
+        next = q.get()
+        if first >= next:
+            result[-1] += 1
+        else:
+            result.append(1)
+            first = next
     return result
 
 
-print(solution(arr))
+print(solution(progresses, speeds))
 
 
-# 1-1. continue 이용 : 조건에 해당하는 경우, 건너뛰기
-def solution2(arr):
+# 2. 인덱스와 뺄셈을 사용해서 몇개가 남았는지 구하기
+def solution2(progresses, speeds):
     result = []
-    for i in arr:
-        if result[-1:] == [i]:
-            continue
-        result.append(i)
-    return result
+    for i in range(len(progresses)):
+        result.append(math.ceil((100 - progresses[i]) / speeds[i]))
+
+    now = 0
+    answer = []
+    for i in range(1, len(progresses)):
+        if result[now] < result[i]:  # 현재보다 더 큰 수를 만나면, 배포
+            answer.append(i - now)
+            now = i
+    # 반복문 조건을 탈출한 경우(현재보다 작은수만 있을 때), now에서의 마지막 배포
+    answer.append(len(progresses) - now)
+    return answer
 
 
-print(solution2(arr))
-
-## 답은 맞았지만, 시간초과된 문제들 : 배열 arr의 크기 : 최대 10^6 까지 될 수므로, 시간복잡도가 n^2인 경우 시간초과됨. 따라서 전체를 비교하는 풀이가 아닌, 스택구조를 사용한 맨 마지막 값만을 비교해여 풀어야 함
-
-
-# 2. if not in 으로 비교
-def solution3(arr):
-    k = []
-    for i in range(len(arr) - 1):
-        if arr[i] == arr[i + 1]:  # 반복문, if문 비교자체로 n(n^2)
-            k.append(i)
-
-    m = []
-    for i in range(len(arr)):
-        if i not in k:
-            m.append(arr[i])
-    return m
-
-
-print(solution3(arr))
-
-
-# 2-1. stack.pop()으로 중복값 빼주기
-def solution4(arr):
-    k = []
-    for i in range(len(arr) - 1):
-        if arr[i] == arr[i + 1]:  # 반복문, if문 비교자체로 n(n^2)
-            k.append(i)
-
-    while k:
-        arr.pop(k[-1])
-        k.pop(-1)
-
-    return arr
-
-
-print(solution4(arr))
+print(solution2(progresses, speeds))
---
+++ algorithm/stack-1.py
@@ -0,0 +1,64 @@
+# 연속으로 중복된 숫자 제거하기
+arr = [1, 1, 3, 3, 0, 1, 1]  # 답 : [1,3,0,1]
+
+
+# 1. stack 이용
+def solution(arr):
+    result = []
+    for i in arr:
+        if result[-1:] != [i]:
+            result.append(i)
+    return result
+
+
+print(solution(arr))
+
+
+# 1-1. continue 이용 : 조건에 해당하는 경우, 건너뛰기
+def solution2(arr):
+    result = []
+    for i in arr:
+        if result[-1:] == [i]:
+            continue
+        result.append(i)
+    return result
+
+
+print(solution2(arr))
+
+## 답은 맞았지만, 시간초과된 문제들 : 배열 arr의 크기 : 최대 10^6 까지 될 수므로, 시간복잡도가 n^2인 경우 시간초과됨. 따라서 전체를 비교하는 풀이가 아닌, 스택구조를 사용한 맨 마지막 값만을 비교해여 풀어야 함
+
+
+# 2. if not in 으로 비교
+def solution3(arr):
+    k = []
+    for i in range(len(arr) - 1):
+        if arr[i] == arr[i + 1]:  # 반복문, if문 비교자체로 n(n^2)
+            k.append(i)
+
+    m = []
+    for i in range(len(arr)):
+        if i not in k:
+            m.append(arr[i])
+    return m
+
+
+print(solution3(arr))
+
+
+# 2-1. stack.pop()으로 중복값 빼주기
+def solution4(arr):
+    k = []
+    for i in range(len(arr) - 1):
+        if arr[i] == arr[i + 1]:  # 반복문, if문 비교자체로 n(n^2)
+            k.append(i)
+
+    while k:
+        # pop()하면서 len이 줄어듦. 큰 인덱스부터 처리위해 거꾸로
+        arr.pop(k[-1])
+        k.pop(-1)
+
+    return arr
+
+
+print(solution4(arr))
