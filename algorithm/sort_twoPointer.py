eabf5634847ce15268103928825b2e631cf8ffd7
knanak <knanak10@gmail.com>
2023-05-05 06:56:00 +0000

    bfs

--- algorithm/sort_twoPointer.py
+++ algorithm/sort_twoPointer.py
@@ -9,27 +9,27 @@ a = list(map(int, stdin.readline().split()))
 x = int(input())
 
 # 1. if in 조건문
-cnt = 0
-for i in a:
+# cnt = 0
+# for i in a:
 
-    if (x - i) in a:
-        cnt += 1
+#     if (x - i) in a:
+#         cnt += 1
 
-print(int(cnt / 2))
+# print(int(cnt / 2))
 
 #2. 정렬후, 투포인터 사용 : 기준이 st
-a.sort()
-st, end, cnt = 0, n - 1, 0
-while st < end:
-    if a[st] + a[end] == x:
-        cnt += 1
-        st += 1
-    elif a[st] + a[end] < x:
-        st += 1
-    else:
-        end -= 1
+# a.sort()
+# st, end, cnt = 0, n - 1, 0
+# while st < end:
+#     if a[st] + a[end] == x:
+#         cnt += 1
+#         st += 1
+#     elif a[st] + a[end] < x:
+#         st += 1
+#     else:
+#         end -= 1
 
-print(cnt)
+# print(cnt)
 
 # 2-1. 투포인터의 기준이 end
 a.sort()
@@ -40,7 +40,7 @@ while st < end:
         cnt += 1
     elif tmp < x:
         st += 1
-        continue
+        continue  ## contine를 써주는 이유
     end -= 1
 
 print(cnt)
