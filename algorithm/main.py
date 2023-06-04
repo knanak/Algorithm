675479c4c4b399709743037bf84d3142ef9261f9
knanak <knanak10@gmail.com>
2023-06-04 01:26:13 +0000

    암호만들기

--- algorithm/main.py
+++ algorithm/main.py
@@ -1,104 +1,62 @@
 # '''
-# n과 m
+# 암호 만들기 : c개 중 최소 한개의 모음 + 최소 두개의 자음으로 n번째로 구성된 암호 만들기
 # '''
-# from sys import stdin
 
-# n, m = 4,2
-# #1. 순열
-# from itertools import permutations
-
-# p=list(permutations([i for i in range(1, n+1)], m))
-# print(p)
-
-# for i in range(1, m+1):
-#   for j in range(1, n+1):
-#     if i!=j:
-#       print(i ,j)
-
-# #2. 백트래킹 : 재귀함수를 돌며, m개를 선택
+# n,c=map(int, input().split())
+# cList=list(map(str, input().split()))
+# cList.sort()
 # result=[]
-# v=[0]*(n+1)
-# def recur(num):
-#   if num==m:      # 재귀함수를 종료하는 조건
-#     print(' '.join(map(str, result)))
-#     return
+# v=['a', 'e', 'i', 'o', 'u']
 
-#   for i in range(1, n+1):
-#     if v[i]==0:
-#       v[i]=1
-#       result.append(i)
-#       recur(num+1)
-
-#       v[i]=0
-#       result.pop()
-
-# recur(0)
-
-# N, M = map(int, input().split())
-# ans = []
-
-# def back():
-#     if len(ans) == M: # 배열의 길이를 확인
-#         print(" ".join(map(str, ans))) # 1 2 3 이런 상태로 출력하기 위해
-#         return 
-#     for i in range(1, N+1): # 1 ~ N 까지
-#         if i not in ans: # 중복 확인
-#             ans.append(i) # 배열 추가
-#             back() # 재귀
-#             print('종료')
-#             ans.pop() # return으로 돌아오면 이게 실행됨. 1, 2, 3 일때 3을 없앰으로 전 단계로 돌아가는 것
-            
-            
-# back()
-
-'''
-n과 m (3) : 1~n까지 m개를 뽑되, 같은 수를 뽑아도 됨
-'''
+# def back(st):
+#   if len(result)== n:
+#     cnt=0
+#     for i in range(n):
+#       if result[i] in v:
+#         cnt+=1
+#         if cnt != n :
+#           print(''.join(result))
+#           return
 
 
-# def back3():
-#   if len(result)==m:
-#     print(' '.join(map(str, result)))
-#     return
-
-#   for i in range(1, n+1):
-#     result.append(i)
-#     back3()
+#   for i in range(st, c) :
+#     result.append(cList[i])
+#     back(i+1)
 #     result.pop()
 
-# back3()
+# back(0)
+
+import sys
 
 
-# def back4(st):
-#   if len(result)== m :
-#     print(' '.join(map(str, result)))
-#     return
+def back_tracking(idx):
 
-#   for i in range(st, n+1):
-#     result.append(i)
-#     back4(i)
-#     result.pop()
+    # 암호를 만들었을 때
+    if len(answer) == l:
+        # 모음, 자음 체크
+        vo, co = 0, 0
 
-# back4(1)
+        for i in answer :
+            if i in consonant:
+                vo += 1
+            else:
+                co += 1
 
-'''
-n과 m 5 : n의 수가 따로 주어짐
-'''
+        # 모음 1개 이상, 자음 2개 이상
+        if vo >= 1 and co >= 2:
+            print("".join(answer))
 
-n,m=map(int, input().split())
-mList=list(map(int, input().split()))
-mList.sort()
-result=[]
+        return
 
-def back5():
-  if len(result)==m:
-    print(" ".join(map(str, result)))
-    return
+    # 반복문을 통해 암호를 만든다.
+    for i in range(idx, c):
+        answer.append(words[i])
+        back_tracking(i + 1) # 백트래킹
+        answer.pop()
 
-  for i in mList :
-    if i not in result :
-      result.append(i)
-      back5()
-      result.pop()
 
-back5()
+l, c = map(int, sys.stdin.readline().split())
+words = sorted(list(map(str, sys.stdin.readline().split())))
+consonant = ['a', 'e', 'i', 'o', 'u']
+answer = []
+back_tracking(0)
