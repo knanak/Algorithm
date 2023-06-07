1550f9ec244e4cb66445459a63c4dca313602984
knanak <knanak10@gmail.com>
2023-06-07 00:53:25 +0000

    로봇청소기

--- algorithm/main.py
+++ algorithm/main.py
@@ -1,62 +1,60 @@
-'''
-차이를 최대로
-'''
+"""
+1. 아이디어
+- while문으로, 특정조건 종료될떄까지 반복
+- 4방향을 for문으로 탐색
+- 더이상 탐색 불가능할경우, 뒤로 한칸 후진
+- 후진이 불가능하면 종료
 
-# n=int(input())
-# nList=list(map(int, input().split())) 
-# result=[]  ## ## nList의 요소가 들어옴
-# sum_max=[]
+2. 시간복잡도
+- O(NM) : 50^2 = 2500 < 2억
+- 가능
 
-# def back(st): ## 인자로는 인덱스가 들어옴
-#   if len(result)==n:
-#     global sum_max
-#     summ=0
-#     for i in range(len(result)-1):
-#       summ+=abs(result[i]-result[i+1])
-      
-#     sum_max.append(summ)
-#     return
+3. 자료구조
+- map : int[][]
+- 로봇청소기 위치, 방향, 전체 청소한 곳 수
 
-#   for i in range(st, n):
-#     if i not in result:
-#       result.append(nList[i])
-#       back(i+1)
-#       result.pop()
-#       print(result)
-
-# back(0)
-# print(max(sum_max))
+"""
 
 import sys
-#sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-n = int(input())
-numbers = list(map(int, input().split()))
-visited = [False] *n
-tmp = []
-ans = []
+N,M = map(int, input().split())
+y,x,d = map(int, input().split())
+map = [list(map(int, input().split())) for _ in range(N)]
+cnt = 0
+dy = [-1,0,1,0]
+dx = [0,1,0,-1]
 
-def dfs(depth):
-  global tmp
-  
-  if depth == n:
-    global ans
-    sum = 0
-    for i in range(len(tmp)-1):
-      sum += abs(tmp[i]-tmp[i+1])
-    
-    ans.append(sum)
-    return
-  
-  for i in range(n):
-    if not visited[i]:
-      visited[i] = True
-      tmp.append(numbers[i])
-      dfs(depth+1)
-      tmp.pop()
-      visited[i] = False
+while 1:
+    if map[y][x] == 0:
+        map[y][x] = 2
+        cnt += 1
+    sw = False
+    # print("d",d)
+    for i in range(1,5):
+        # print("d-i", d-i)
+        ny = y + dy[d-i]
+        nx = x + dx[d-i]
+        if 0<=ny<N and 0<=nx<M:
+            if map[ny][nx] == 0:
+                d = abs((d-i) %4)
+                y = ny
+                x = nx
+                sw = True
+                break
 
+    # 4방향 모두 있지 않은경우
+    if sw == False:
+        # 뒤쪽 방향이 막혀있는 지확인
+        ny = y - dy[d]
+        nx = x - dx[d]
+        if 0 <= ny < N and 0 <= nx < M:
+            if map[ny][nx] == 1:
+                break
+            else:
+                y = ny
+                x = nx
+        else:
+            break
 
-dfs(0)
-print(max(ans))
+print(cnt)
